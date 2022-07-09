52e71303a67eadffde033063196f7ccb754c9a12
Lucas <lucaslcslcs1998@gmail.com>
2022-07-09 14:54:10 -0300

    Mockando nossas Noticias para o RecyclerView

--- app/src/main/java/me/dio/soccernews/ui/news/NewsFragment.java
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -24,9 +23,10 @@ public class NewsFragment extends Fragment {
         binding = FragmentNewsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNews;
-        NewsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        NewsViewModel.getNews().observe(getViewLifecycleOwner(), news ->{
+
+        });
+            return root;
     }
 
     @Override
--- app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
@@ -1,19 +1,31 @@
 package me.dio.soccernews.ui.news;
 
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NewsViewModel extends ViewModel {
+import java.util.ArrayList;
+import java.util.List;
 
-    private final MutableLiveData<String> mText;
+import me.dio.soccernews.domain.News;
+
+public class NewsViewModel extends ViewModel{
+
+    private final MutableLiveData<List<News>> news;
 
     public NewsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        this.news = new MutableLiveData<>();
+
+        //TODO Remover Mock de Noticias
+        List<News> news = new ArrayList<>();
+        news.add(new News("Ferroviaria tem desfalque importante",""));
+        news.add(new News("Ferrinha jogo sabado",""));
+        news.add(new News( "Copa do Mundo Feminina esta terminando",""));
+
+        this.news.setValue(news);
+
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public MutableLiveData<List<News>> getNews() {
+        return this.news;
     }
 }
