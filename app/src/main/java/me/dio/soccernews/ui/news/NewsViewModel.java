0e09b3af1e5cad462226e1d85f16934813d1442f
Lucas <lucaslcslcs1998@gmail.com>
2022-07-13 17:36:45 -0300

    Classes de dominio e consumo de api via Retrofit

--- app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
@@ -3,26 +3,46 @@ package me.dio.soccernews.ui.news;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import me.dio.soccernews.data.remote.SoccerNewsApi;
 import me.dio.soccernews.domain.News;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NewsViewModel extends ViewModel{
 
-    private final MutableLiveData<List<News>> news;
+    private final MutableLiveData<List<News>> news = new MutableLiveData<>();
+    private final SoccerNewsApi api;
 
     public NewsViewModel() {
-        this.news = new MutableLiveData<>();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://lucaslcslcs1998.github.io/soccer-news-api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-        //TODO Remover Mock de Noticias
-        List<News> news = new ArrayList<>();
-        news.add(new News("Ferroviaria tem desfalque importante","aa"));
-        news.add(new News("Ferrinha jogo sabado","aa"));
-        news.add(new News("Copa do Mundo Feminina esta terminando","aa"));
+        api = retrofit.create(SoccerNewsApi.class);
+        this.findNews();
+    }
 
-        this.news.setValue(news);
+    private void findNews() {
+        api.getNews().enqueue(new Callback<List<News>>() {
+            @Override
+            public void onResponse(Call<List<News>> call, Response<List<News>> response) {
+                if (response.isSuccessful()){
+                    news.setValue(response.body());
+                }//TODO Pensar em uma estrategia de tratamentos de erros
 
+            }
+
+            @Override
+            public void onFailure(Call<List<News>> call, Throwable t) {
+                //TODO Pensar em uma estrategia de tratamentos de erros
+            }
+        });
     }
 
     public MutableLiveData<List<News>> getNews() {
